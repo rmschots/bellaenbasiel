1f3cc0ec8f65805fd71f0eecbbc45322f3a90012
Roel <mangelschotsroel@hotmail.com>
2018-04-20 00:15:05 +0200

    finalize cloud function for refreshing calendar

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,20 +1,49 @@
-const functions = require('firebase-functions');
+import { CalendarEntry } from './calendar-entry';
 import * as rp from 'request-promise';
+import * as ICAL from 'ical.js';
+
+const functions = require('firebase-functions');
 
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
 
-exports.refreshAirbnbCaledendar = functions.https.onRequest((req, res) => {
+function parseCalendar(calData): CalendarEntry[] {
+  const parsed = ICAL.parse(calData);
+  const comp = new ICAL.Component(parsed);
+  const vevents = comp.getAllSubcomponents('vevent');
+  return vevents.map(vevent => {
+    const event = new ICAL.Event(vevent);
+    const startDate: Date = event.startDate.toJSDate();
+    const endDate: Date = event.endDate.toJSDate();
+    endDate.setDate(endDate.getDate() - 1);
+
+    return {
+      startDate: startDate,
+      endDate: endDate
+    } as CalendarEntry;
+  });
+}
+
+function saveCalendarEntries(calendarEntries: CalendarEntry[]): Promise<any> {
+  console.info('saving ' + calendarEntries.length + ' calendar entries');
+  return db.collection('data').doc('calendar').set({entries: calendarEntries});
+}
+
+exports.refreshAirbnbCalendar = functions.https.onRequest((req, res) => {
   rp({
-    uri: 'https://www.airbnb.be/calendar/ical/12102270.ics?s=43d614d026f02aef4ac8431f625b3433',
+    uri: 'https://calendar.google.com/calendar/ical/a2cpg0o6c8rfv2479ga1fkshe9g34ahh%40import.calendar.google.com/public/basic.ics',
     method: 'GET'
-  }).then(result => {
-    if (result.success) {
-      console.info('successfully fetched airbnb data: ', JSON.stringify(result));
-      res.status(200).send('refresh successful');
-    } else {
-      console.error('fetching airbnb calendar failed: ', JSON.stringify(result));
-      res.status(200).send('refresh failed: ' + JSON.stringify(result));
-    }
+  }).then(calData => {
+    const calendarEntries = parseCalendar(calData);
+    saveCalendarEntries(calendarEntries).then(() => {
+      res.status(200).send('saving calendar data complete');
+    }).catch(error => {
+      console.error('could not save calendar data: ', JSON.stringify(error));
+      res.status(200).send('could not save calendar data: ' + JSON.stringify(error));
+    });
+  }).catch(error => {
+    console.error('fetching airbnb calendar failed: ', JSON.stringify(error));
+    res.status(200).send('refresh failed: ' + JSON.stringify(error));
   })
 });
