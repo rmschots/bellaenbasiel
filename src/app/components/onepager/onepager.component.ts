e38a0338a3395b7852a0523e478ba9aef4c77183
Roel <mangelschotsroel@hotmail.com>
2018-06-03 12:46:36 +0200

    replace NgxGallery with ModalGallery in pictures section

--- src/app/components/onepager/onepager.component.ts
+++ src/app/components/onepager/onepager.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FirebaseService } from '../../shared/services/firebase.service';
 import { ObservableMedia } from '@angular/flex-layout';
 
 @Component({
   selector: 'bnb-onepager',
   templateUrl: './onepager.component.html',
-  styleUrls: ['./onepager.component.scss']
+  styleUrls: ['./onepager.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class OnepagerComponent implements OnInit {
 
