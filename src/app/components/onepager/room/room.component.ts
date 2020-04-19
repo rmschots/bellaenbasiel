99b496dcf9083a193abfebcd31bc1f8308192fd5
rmschots <mangelschotsroel@hotmail.com>
2020-04-19 15:43:25 +0800

    change prices and extra info

--- src/app/components/onepager/room/room.component.ts
+++ src/app/components/onepager/room/room.component.ts
@@ -6,6 +6,7 @@ import { Unsubscribable } from '../../../shared/util/unsubscribable';
 import { FirebaseCalendar } from '../../../shared/models/firebase-data';
 import { BehaviorSubject } from 'rxjs';
 import { distinctUntilChanged, filter, takeUntil } from 'rxjs/operators';
+import { RoomConfig } from '../../../shared/models/room-config';
 
 @Component({
   selector: 'bnb-room',
@@ -14,42 +15,52 @@ import { distinctUntilChanged, filter, takeUntil } from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RoomComponent extends Unsubscribable {
-  images: Image[] = [
-    new Image(1, {
-      img: '/assets/img/rooms/room1/bedroom.jpg',
-      description: 'ROOM.rooms.room1.image1'
-    }, { img: '/assets/img/rooms/room1/bedroom.jpg' }),
-    new Image(2, {
-      img: '/assets/img/rooms/room1/bathroom.jpg',
-      description: 'ROOM.rooms.room1.image2'
-    }, { img: '/assets/img/rooms/room1/bathroom.jpg' }),
-    new Image(3, {
-      img: '/assets/img/rooms/breakfast.jpg',
-      description: 'ROOM.rooms.room1.image3'
-    }, { img: '/assets/img/rooms/breakfast.jpg' })
-  ];
-
-  images2: Image[] = [
-    new Image(4, {
-      img: '/assets/img/rooms/room2/bedroom.jpg',
-      description: 'ROOM.rooms.room2.image1'
-    }, { img: '/assets/img/rooms/room2/bedroom.jpg' }),
-    new Image(6, {
-      img: '/assets/img/rooms/breakfast.jpg',
-      description: 'ROOM.rooms.room2.image3'
-    }, { img: '/assets/img/rooms/breakfast.jpg' })
-  ];
-
-  images3: Image[] = [
-    new Image(7, {
-      img: '/assets/img/rooms/room3/bedroom.jpg',
-      description: 'ROOM.rooms.room3.image1'
-    }, { img: '/assets/img/rooms/room3/bedroom.jpg' }),
-    new Image(9, {
-      img: '/assets/img/rooms/breakfast.jpg',
-      description: 'ROOM.rooms.room3.image3'
-    }, { img: '/assets/img/rooms/breakfast.jpg' })
-  ];
+  roomConfig1: RoomConfig = {
+    id: 1,
+    price: 80,
+    images: [
+      new Image(1, {
+        img: '/assets/img/rooms/room1/bedroom.jpg',
+        description: 'ROOM.rooms.room1.image1'
+      }, {img: '/assets/img/rooms/room1/bedroom.jpg'}),
+      new Image(2, {
+        img: '/assets/img/rooms/room1/bathroom.jpg',
+        description: 'ROOM.rooms.room1.image2'
+      }, {img: '/assets/img/rooms/room1/bathroom.jpg'}),
+      new Image(3, {
+        img: '/assets/img/rooms/breakfast.jpg',
+        description: 'ROOM.rooms.room1.image3'
+      }, {img: '/assets/img/rooms/breakfast.jpg'})
+    ]
+  };
+  roomConfig2: RoomConfig = {
+    id: 2,
+    price: 85,
+    images: [
+      new Image(4, {
+        img: '/assets/img/rooms/room2/bedroom.jpg',
+        description: 'ROOM.rooms.room2.image1'
+      }, {img: '/assets/img/rooms/room2/bedroom.jpg'}),
+      new Image(6, {
+        img: '/assets/img/rooms/breakfast.jpg',
+        description: 'ROOM.rooms.room2.image3'
+      }, {img: '/assets/img/rooms/breakfast.jpg'})
+    ]
+  };
+  roomConfig3: RoomConfig = {
+    id: 3,
+    price: 75,
+    images: [
+      new Image(7, {
+        img: '/assets/img/rooms/room3/bedroom.jpg',
+        description: 'ROOM.rooms.room3.image1'
+      }, {img: '/assets/img/rooms/room3/bedroom.jpg'}),
+      new Image(9, {
+        img: '/assets/img/rooms/breakfast.jpg',
+        description: 'ROOM.rooms.room3.image3'
+      }, {img: '/assets/img/rooms/breakfast.jpg'})
+    ]
+  };
 
   private _firebaseCalendar1$: BehaviorSubject<FirebaseCalendar> = new BehaviorSubject<FirebaseCalendar>(undefined);
   private _firebaseCalendar2$: BehaviorSubject<FirebaseCalendar> = new BehaviorSubject<FirebaseCalendar>(undefined);
---
+++ src/app/shared/models/room-config.ts
@@ -0,0 +1,7 @@
+import { Image } from '@ks89/angular-modal-gallery';
+
+export interface RoomConfig {
+  id: number;
+  images: Image[];
+  price: number;
+}
