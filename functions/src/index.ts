94ee0dabc143a22e335e0b667413970bdc303409
Roel Mangelschots <mangelschotsroel@gmail.com>
2024-12-17 13:04:12 +0800

    update calendar functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,20 @@
 import { CalendarEntry } from './calendar-entry';
 import * as rp from 'request-promise';
-import * as ICAL from 'ical.js';
+import * as functions from 'firebase-functions';
 
-const functions = require('firebase-functions');
+import { initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
 
-const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
-const db = admin.firestore();
+const ICAL = require('ical.js');
 
-function parseCalendar(calData): CalendarEntry[] {
+initializeApp();
+const db = getFirestore('firestore-eu');
+
+function parseCalendar(calData: any): CalendarEntry[] {
   const parsed = ICAL.parse(calData);
   const comp = new ICAL.Component(parsed);
   const vevents = comp.getAllSubcomponents('vevent');
-  return vevents.map(vevent => {
+  return vevents.map((vevent: any) => {
     const event = new ICAL.Event(vevent);
     const startDate: Date = event.startDate.toJSDate();
     const endDate: Date = event.endDate.toJSDate();
@@ -30,7 +32,7 @@ function saveCalendarEntries(calendarEntries: CalendarEntry[], calendarDoc: stri
   return db.collection('data').doc(calendarDoc).set({entries: calendarEntries});
 }
 
-exports.refreshAirbnbCalendar = functions.https.onRequest((req, res) => {
+exports.refreshAirbnbCalendar = functions.https.onRequest({region: 'europe-west1'}, (req, res) => {
   rp({
     uri: 'https://calendar.google.com/calendar/ical/a2cpg0o6c8rfv2479ga1fkshe9g34ahh%40import.calendar.google.com/public/basic.ics',
     method: 'GET'
@@ -39,16 +41,16 @@ exports.refreshAirbnbCalendar = functions.https.onRequest((req, res) => {
     saveCalendarEntries(calendarEntries, 'calendar').then(() => {
       res.status(200).send('saving calendar data complete');
     }).catch(error => {
-      console.error('could not save calendar data: ', JSON.stringify(error));
+      console.error('could not save calendar data: ', error, JSON.stringify(error));
       res.status(200).send('could not save calendar data: ' + JSON.stringify(error));
     });
   }).catch(error => {
-    console.error('fetching airbnb calendar failed: ', JSON.stringify(error));
-    res.status(200).send('refresh failed: ' + JSON.stringify(error));
-  })
+    console.error('fetching airbnb calendar faileddd: ', error, JSON.stringify(error));
+    res.status(200).send('refresh failedddd: ' + JSON.stringify(error));
+  });
 });
 
-exports.refreshAirbnbCalendar2 = functions.https.onRequest((req, res) => {
+exports.refreshAirbnbCalendar2 = functions.https.onRequest({region: 'europe-west1'}, (req, res) => {
   rp({
     uri: 'https://calendar.google.com/calendar/ical/lo809anhm07epg6v2ocgb58567lo70hs%40import.calendar.google.com/public/basic.ics',
     method: 'GET'
@@ -63,5 +65,5 @@ exports.refreshAirbnbCalendar2 = functions.https.onRequest((req, res) => {
   }).catch(error => {
     console.error('fetching airbnb calendar2 failed: ', JSON.stringify(error));
     res.status(200).send('refresh2 failed: ' + JSON.stringify(error));
-  })
+  });
 });
