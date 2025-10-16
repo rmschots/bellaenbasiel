4129460df5be0f9f3277c47ddfda20770eeaeadf
Roel Mangelschots <mangelschotsroel@gmail.com>
2025-10-16 10:58:28 +0800

    update room prices and clarify deposit account details in translations

--- src/app/components/onepager/room/room.component.ts
+++ src/app/components/onepager/room/room.component.ts
@@ -17,7 +17,7 @@ import { isEqual } from 'lodash';
 export class RoomComponent {
   roomConfig1: RoomConfig = {
     id: 1,
-    price: 105,
+    price: 108,
     images: [
       new Image(1, {
         img: '/assets/img/rooms/room1/bedroom.jpg',
@@ -35,7 +35,7 @@ export class RoomComponent {
   };
   roomConfig2: RoomConfig = {
     id: 2,
-    price: 110,
+    price: 118,
     images: [
       new Image(4, {
         img: '/assets/img/rooms/room2/bedroom.jpg',
@@ -53,7 +53,7 @@ export class RoomComponent {
   };
   roomConfig3: RoomConfig = {
     id: 3,
-    price: 100,
+    price: 98,
     images: [
       new Image(7, {
         img: '/assets/img/rooms/room3/bedroom.jpg',
